ebe33ccc8065a70c120f3dbdf73909629410ed44
adricosmicfox <adriiphone@gmail.com>
2022-09-03 18:54:44 +0200

    completed con pari e dispari part

--- js/script.js
+++ js/script.js
@@ -1,52 +1,74 @@
-// Palindroma
-// Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
-// Pari e Dispari
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
-// Consigli del giorno
-// 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
-// 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+// creare input per chiedere all'utente di inserire parola
+const form = document.getElementById("am-form");
+
+// aggiunta funzione al bottone per verifica parola
+form.addEventListener("submit", verificaPal);
+form.addEventListener("submit", pariEDispari);
 
 
+function verificaPal() {
+
+    let parolaUtente = document.getElementById("box-parola").value;
+    console.log(parolaUtente);
+
+    // split crea automaticamente un array dividendo le lettere
+    let splitParola = parolaUtente.split("");
+
+    console.log(splitParola);
+
+    // creare una funzione per capire se la parola è palindroma
+    let i;
+
+    // creiamo un array vuoto per inserire la parola al contrario
+    let parolaRigirata = [];
+    // creiamo il contatore per parolaRigirata
+    let j = 0;
 
 
+    // prendiamo l'ultima lettere dalla parola utente e la inseriamo nel nuovo array nella prima posizione e così via per le posizioni e lettere successive
+    for (i = splitParola.length - 1; i >= 0; i--) {
 
-// creare prompt per chiedere all'utente di inserire parola
+        parolaRigirata[j] = splitParola[i];
+        j++;
+        console.log(parolaRigirata);
+    }
 
-let parolaUtente = window.prompt("Inserisci una parola");
+    if (parolaUtente == "") {
+        alert("inserisci una parola")
+    }
 
-// split crea automaticamente un array dividendo le lettere
-let splitParola = parolaUtente.split("");
+    // join unisce gli elementi contenuti nell'array eliminando le virgole
+    else if (parolaRigirata.join("") == parolaUtente) {
+        alert("la parola è palindroma");
 
-console.log(splitParola);
-
-// creare una funzione per capire se la parola è palindroma
-let i;
-
-// creiamo un array vuoto per inserire la parola al contrario
-let parolaRigirata = [];
-// creiamo il contatore per parolaRigirata
-let j = 0;
-
-
-// prendiamo l'ultima lettere dalla parola utente e la inseriamo nel nuovo array nella prima posizione e così via per le posizioni e lettere successive
-for (i = splitParola.length - 1; i >= 0; i--) {
-
-    parolaRigirata[j] = splitParola[i];
-    j++;
-    console.log(parolaRigirata);
+    }
+    else {
+        console.log("no");
+        alert("la parola NON è palindroma");
+    }
 }
 
-// join unisce gli elementi contenuti nell'array eliminando le virgole
-if (parolaRigirata.join("") == parolaUtente) {
-    alert("la parola è palindroma");
-}
-else {
-    alert("la parola NON è palindroma");
-}
 
-// restituire messaggio se la parola è palindroma o no
+
+function pariEDispari() {
+
+    let numeroUtente = parseInt(document.getElementById("box-numero").value);
+
+    let numeroRandom = parseInt(Math.floor(Math.random() * 5) + 1);
+    console.log(numeroRandom);
+
+    let somma = numeroUtente + numeroRandom;
+    console.log(somma);
+
+    // se pari
+    if (somma % 2 == 0) {
+        alert("il numero è pari");
+
+    }
+    // se dispari
+    else {
+        alert("il numero è dispari")
+    }
+
+}
